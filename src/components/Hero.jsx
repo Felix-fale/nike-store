af477124a944bb30b1d92a03b0299e26b187b1b9
Felix-fale <falefelix23@gmail.com>
2022-12-12 21:40:13 +0000

    feat: install @heroicons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import Clips from './utils/Clips';
+import SocialLink from './utils/SocialLink';
 
-function Hero({ heroapi: { title, subtitle, btntext, img, siciallinks, videos } }) {
+function Hero({ heroapi: { title, subtitle, btntext, img, sociallinks, videos } }) {
     // console.log(heroapi);
     return (
         <>
@@ -10,12 +12,29 @@ function Hero({ heroapi: { title, subtitle, btntext, img, siciallinks, videos } 
                     <div className=' grid items-center justify-items-center mt-28 md:mt-24'>
                         <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter text-slate-200 '>{title} </h1>
                         <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter text-slate-200 '>{subtitle} </h1>
-                        <button>{btntext} </button>
-                        {/* <div className=''></div>
-                        <div className=''></div> */}
+                        <button type='button' className='button-theme bg-slate-200 shadow-slate-200 rounded-xl my-5' >
+                            {btntext}
+                        </button>
+                        <div className='grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto '>
+                            {videos?.map((val, i) => (
+                                <Clips
+                                    key={i}
+                                    imgsrc={val.imgsrc}
+                                    clip={val.clip}
+                                />
+                            ))}
+                        </div>
+                        <div className=''>
+                        {sociallinks?.map((val, i) => (
+                                <SocialLink
+                                    key={i}
+                                    icon={val.icon}
+                                />
+                            ))}
+                        </div>
                     </div>
-                    <div className=''>
-                        <img src={img} alt="hero-img/img" className='' />
+                    <div className='flex items-center'>
+                        <img src={img} alt="hero-img/img" className='w-auto h-[45vh] lg:h-[35vh] md:h-[31vh] sm:h-[21vh] xsm:h-[19vh] transitions-theme -rotate-[25deg] hover:rotate-0 cursor-pointer object-fill ' />
                     </div>
                 </div>
             </div>
