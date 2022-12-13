8148e3d07d2d28b47f5689db120597a2ee7c3863
Felix-fale <falefelix23@gmail.com>
2022-12-13 09:04:52 +0000

    feat: add new styles to tpopular sales

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -10,7 +10,7 @@ function Sales({ ifExists, endpoint: { title, items } }) {
                 <Title title={title} />
                 <div className={`grid items-center justify-items-center gap-7 lg:gap-5 mt-7 ${ifExists? 'grid-cols-3 xl:grid-cols-2 sm:grid-cols-1' : 'grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'} `}>
                     {items?.map((item, i) => (
-                        <Item {...item} key={i} />
+                        <Item {...item} key={i} ifExists={ifExists} />
                     ))}
                 </div>
             </div>
--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,12 +1,12 @@
 import { StarIcon, ShoppingBagIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
-function Item({ id, color, shadow, title, text, img, btn, rating, price }) {
+function Item({ ifExists, id, color, shadow, title, text, img, btn, rating, price }) {
     console.log(id);
     return (
         <>
-            <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105 `}>
-                <div className='grid items-center justify-items-center'>
+            <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center ${ifExists ? 'justify-items-start' : ' justify-items-center'} rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105 `}>
+                <div className={`grid items-center ${ifExists ? 'justify-items-start' : ' justify-items-center'}`}>
                     <h1 className='text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow '>{title} </h1>
                     <p className='text-slate-200 filter drop-shadow text-base md:text-sm font-normal'>{text} </p>
 
@@ -31,8 +31,11 @@ function Item({ id, color, shadow, title, text, img, btn, rating, price }) {
                         </button>
                     </div>
                 </div>
-                <div className='flex items-center'>
-                    <img src={img} alt="img/item-img" className='h-36 w-64 transition-theme hover:-rotate-12' />
+                <div className={`flex items-center ${ifExists ? 'absolute top-5 right-1' : ' justify-center'}`}>
+                    <img src={img}
+                        alt={`img/item-img/${id} `}
+                        className={`transition-theme hover:-rotate-12 ${ifExists ? 'h-auto w-64 lg:w-56 md:w-48 -rotate-[35deg] ' : ' h-36 w-64 '}`}
+                    />
                 </div>
             </div>
         </>
