92ff1eb86f671533610ee77418c3c19b8532b449
Felix-fale <falefelix23@gmail.com>
2022-12-14 01:50:43 +0000

    refactor:  add extension JSX to all import in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import {default as Hero}  from "./Hero.jsx";
-import {default as Sales} from "./Sales";
-import {default as FlexContent} from "./FlexContent";
-import {default as Stories} from "./Stories";
+import {default as Sales} from "./Sales.jsx";
+import {default as FlexContent} from "./FlexContent.jsx";
+import {default as Stories} from "./Stories.jsx";
+import {default as Footer} from "./Footer.jsx";
