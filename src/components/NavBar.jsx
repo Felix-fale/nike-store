2ca72d77b6620c5405ef40ef3db07dc37da5ceca
Felix-fale <falefelix23@gmail.com>
2022-12-14 22:09:22 +0000

    feat: navbar styling done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,7 @@ function NavBar() {
     return (
         <>
             <header className={
-                !navState ? 'absolute top-7 left-0 right-0 opacity-100 z-50' : 'fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-[100] blur-effect-theme'
+                !navState ? 'absolute top-7 left-0 right-0 opacity-100 z-50' : 'fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-[200] blur-effect-theme'
             }>
                 <nav className='flex items-center justify-between nike-container'>
                     <div className='flex items-center'>
@@ -33,15 +33,16 @@ function NavBar() {
                     </div>
                     <ul className='flex items-center justify-center gap-2'>
                         <li className='grid items-center'>
-                            <MagnifyingGlassIcon className='icon-style' />
+                            <MagnifyingGlassIcon className={`icon-style ${navState && "text-slate-900 translate-all duration-300"}`} />
                         </li>
                         <li className='grid items-center'>
-                            <HeartIcon className='icon-style' />
+                            <HeartIcon className={`icon-style ${navState && "text-slate-900 translate-all duration-300"}`} />
                         </li>
                         <li className='grid items-center'>
                             <button type='button' className='border-none outline-none active:scale-110 transition-all duration-300 relative'>
-                                <ShoppingBagIcon className='icon-style' />
-                                <div className={`absolute top-4 right-0 bg-white text-slate-900 shadow shadow-slate-100  w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300`}>0</div>
+                                <ShoppingBagIcon className={`icon-style ${navState && "text-slate-900 translate-all duration-300"}`} />
+
+                                <div className={`absolute top-4 right-0 shadow   w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 ${navState ? 'bg-slate-900 text-slate-100 shadow-slate-900' : 'bg-slate-100 text-slate-900 shadow-slate-100'} `}>0</div>
                             </button>
                         </li>
                     </ul>
