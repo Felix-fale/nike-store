10482dfa28c6ddf343e08ab715b20aadb53448b1
Felix-fale <falefelix23@gmail.com>
2022-12-19 11:14:33 +0000

    feat: add button buy Now functionality

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { StarIcon, ShoppingBagIcon } from '@heroicons/react/24/solid';
-import { setAddItemToCart } from '../app/CartSlice';
+import { setAddItemToCart, setOpenCart } from '../app/CartSlice';
 
 function Item({ ifExists, id, color, shadow, title, text, img, btn, rating, price }) {
 
@@ -12,6 +12,12 @@ function Item({ ifExists, id, color, shadow, title, text, img, btn, rating, pric
         const item = { id, color, shadow, title, text, img, btn, rating, price };
 
         dispatch(setAddItemToCart(item));
+    }
+
+    const onCartToggle = () => {
+        dispatch(setOpenCart({
+            cartState: true
+        }))
     }
 
     return (
@@ -41,7 +47,10 @@ function Item({ ifExists, id, color, shadow, title, text, img, btn, rating, pric
                         >
                             <ShoppingBagIcon className='icon-style text-slate-900' />
                         </button>
-                        <button type='button' className='bg-white/90 blur-effect-theme button-theme px-2 py-1 shadow shadow-slate-200 text-sm text-black'>
+                        <button type='button'
+                            className='bg-white/90 blur-effect-theme button-theme px-2 py-1 shadow shadow-slate-200 text-sm text-black'
+                            onClick={() => { onAddToCart(); onCartToggle(); }}
+                        >
                             {btn}
                         </button>
                     </div>
