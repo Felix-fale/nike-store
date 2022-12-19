41aa90c4012390e2ad2b75ce8f31c0ab8d0940e9
Felix-fale <falefelix23@gmail.com>
2022-12-19 11:07:34 +0000

    feat: add onCartToggle to CartEmpty

--- src/components/Carts.jsx
+++ src/components/Carts.jsx
@@ -30,7 +30,7 @@ function Carts() {
                 <div className={`blur-effect-theme h-screen max-w-xl w-full absolute right-0`}>
                     <CartCount onCartToggle={onCartToggle} onClearCartItems={onClearCartItems} />
 
-                    {cartItems?.length === 0 ? <CartEmpty /> : <div>
+                    {cartItems?.length === 0 ? <CartEmpty onCartToggle={onCartToggle} /> : <div>
                         <div className='flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[81vh] scroll-smooth scroll-hidden py-3'>
                             {cartItems?.map((item, i) => (
                                 <CartItem key={i} item={item} />
